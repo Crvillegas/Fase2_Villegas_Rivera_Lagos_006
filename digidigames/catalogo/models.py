2a65ea864481c36d23a0aa38936ff81e65afafa8
cr.villegasg1 <cr.villegasg1@gmail.com>
2020-10-24 00:26:10 -0300

    VideoJuegos:Creando Modelos

--- digidigames/catalogo/models.py
+++ digidigames/catalogo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.urls import reverse  #Redirecciona una url de un libro al browser 
+import uuid                      #se utiliza para definir atributos clave (pk)
 # Create your models here.
+
+class juego(models.Model):
+    
+    id_juego=models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Atributo unico para este juego en particular')
+    titulo = models.CharField(max_length=200)
+    descipcion= models.TextField(max_length=1000)
+    precio=models.CharField(max_length=6)
+    plataforma_status=(
+        ('S','Steam'),
+        ('O','Origin'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=plataforma_status,
+        blank=True,
+        default='S',
+        help_text='Plataforma del juego',
+    )
+
+
+            
+
