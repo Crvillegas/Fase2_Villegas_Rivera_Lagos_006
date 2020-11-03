10029928a9b33d4ad0716c52da73f33bc34187b8
cr.villegasg1 <cr.villegasg1@gmail.com>
2020-11-03 01:15:52 -0300

    VideoJuegos: Creando Formularios

--- digidigames/catalogo/urls.py
+++ digidigames/catalogo/urls.py
@@ -3,4 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Contacto/', views.Contacto, name='Contacto'),
+    path('Steam/', views.Steam, name='Steam'),
+    path('Origin/', views.Origin, name='Origin'),
+    path('Pago/', views.Pago, name='Pago'),
+    path('juego/<int:pk>', views.juegoDetailView.as_view(), name='juego-detail')
+]
+
+urlpatterns+= [
+    path('juego/create/', views.juegoCreate.as_view(), name='juego_create'),
+    path('juego/<int:pk>/update/', views.juegoUpdate.as_view(), name='juego_update'),
+    path('juego/<int:pk>/delete/', views.juegoDelete.as_view(), name='juego_delete')
 ]
--- digidigames/catalogo/views.py
+++ digidigames/catalogo/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from . models import juego
 from django.views import generic
 
+#Importamos informacion para formularios 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 # Create your views here.
 def index(request) :
     num_juego = juego.objects.all().count()
@@ -11,4 +15,52 @@ def index(request) :
         'index.html',
         context={'num_juego':num_juego},
     )
-    
+def Contacto(request) :
+    num_juego = juego.objects.all().count()
+
+    return render(
+        request,
+        'Contacto.html',
+        context={'num_juego':num_juego},
+    )
+def Steam(request) :
+    num_juego = juego.objects.all().count()
+
+    return render(
+        request,
+        'Steam.html',
+        context={'num_juego':num_juego},
+    )
+def Origin(request) :
+    num_juego = juego.objects.all().count()
+
+    return render(
+        request,
+        'Origin.html',
+        context={'num_juego':num_juego},
+    )
+def Pago(request) :
+    num_juego = juego.objects.all().count()
+
+    return render(
+        request,
+        'Pago.html',
+        context={'num_juego':num_juego},
+    )
+
+#crud
+
+class juegoCreate(CreateView):
+    model = juego
+    fields = '__all__'
+
+class juegoUpdate(UpdateView):
+    model = juego
+    fields = ['titulo', 'descipcion', 'precio', 'plataforma_status']
+
+class juegoDelete(DeleteView):
+    model = juego
+    success_url = reverse_lazy ('juego')
+
+class juegoDetailView(generic.DetailView):
+    model = juego
